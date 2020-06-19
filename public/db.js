ba41560e659fb6419febe018719c7d9d37273538
Jordan Noe <jordanvinnoe@gmail.com>
2020-06-19 10:04:43 -0400

    testing PORT setting

--- public/db.js
+++ public/db.js
@@ -2,7 +2,8 @@ let db;
 // create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
+  console.log("this.onupgradeneeded");
    // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
